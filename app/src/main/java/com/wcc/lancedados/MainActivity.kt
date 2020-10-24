60b9682336a4ba57061f43c5e3d23a2cccdb7799
Anna Sahu <73133104+annasahu@users.noreply.github.com>
2020-10-24 10:12:55 -0300

    gerando os dados aleatoriamente

--- app/src/main/java/com/wcc/lancedados/MainActivity.kt
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wcc.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +11,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* variaveis */
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadoBotao)
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
 
-
-        /* botao para lancar dados */
         botaoLancarDados.setOnClickListener {
+            dado1.setImageResource(lancarDados())
+            dado2.setImageResource(lancarDados())
         }
     }
 
-    /* retornar valor aleatorio entre 1 e 6 */
     fun lancarDados(): Int {
+        return recuperaFaceDado(gerarNumero())
+    }
+
+    fun recuperaFaceDado(numero: Int): Int {
+        return when(numero) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    fun gerarNumero(): Int {
         return (1..6).random()
     }
 }
